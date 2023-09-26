873356104df9bf5b64c4bcb949a3147680ab9b92
Lukas <45790416+lukasjoc@users.noreply.github.com>
2023-09-26 22:45:40 +0200

    feat: rewrite fritz python script into a self-contained go binary
    Get info, reconnect and reboot are implemented. Currently, only be reverse-engineering the requests from the web interface. All of this works on my machine. I don't guarantee that it works for anyone else.
    /internal contains sources from https://github.com/philippfranke/go-fritzbox/tree/master/fritzbox. Thanks for this!

---
+++ fritz/go.mod
@@ -0,0 +1,5 @@
+module github.com/lukasjoc/scripts/fritz
+
+go 1.21.0
+
+require gopkg.in/yaml.v2 v2.4.0
---
+++ fritz/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"github.com/lukasjoc/scripts/fritz/internal"
+	"os"
+)
+
+func printHelp() {
+    fmt.Fprintf(os.Stderr, "\n%s [command]\n", os.Args[0])
+    fmt.Fprintf(os.Stderr, "%-10s %s\n", "info", "Print info about the box and its configuration")
+    fmt.Fprintf(os.Stderr, "%-10s %s\n", "reconnect", "Quickly disconnect and reconnect again")
+    fmt.Fprintf(os.Stderr, "%-10s %s\n", "reboot", "Quickly reboot the box")
+	os.Exit(1)
+}
+
+func main() {
+	if len(os.Args) < 2 {
+		printHelp()
+	}
+
+	fritz, err := internal.NewFritz()
+	if err != nil {
+        panic(err)
+	}
+	switch os.Args[1] {
+	case "reboot":
+		if err := fritz.Reboot(); err != nil {
+			panic(err)
+		}
+	case "reconnect":
+		if err := fritz.Reconnect(); err != nil {
+			panic(err)
+		}
+	case "info":
+		if err := fritz.Info(); err != nil {
+			panic(err)
+		}
+	default:
+		printHelp()
+	}
+}
