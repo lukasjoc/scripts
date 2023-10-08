b29c64f341eb180c11e942925c5fd1622fd7785e
lukasjoc <45790416+lukasjoc@users.noreply.github.com>
2023-10-08 19:07:56 +0200

    gitstatus: make it work fater

--- jap/hiragana.go
+++ jap/hiragana.go
@@ -1,47 +1,39 @@
 package main
 
-import (
-	"fmt"
-	"math/rand"
-)
-
-var hiragana = map[string]string{
-	"a":   "あ",
-	"i":   "い",
-	"u":   "う",
-	"e":   "え",
-	"o":   "お",
-	"ka":  "か",
-	"ki":  "き",
-	"ku":  "く",
-	"ke":  "け",
-	"ko":  "こ",
-	"sa":  "さ",
-	"shi": "し",
-	"su":  "す",
-	"se":  "せ",
-	"so":  "そ",
-}
-
-func getRandomHiragana() (string, string) {
-	keys := make([]string, 0, len(hiragana))
-	for k := range hiragana {
-		keys = append(keys, k)
-	}
-	randomIndex := rand.Intn(len(keys))
-	k := keys[randomIndex]
-	return k, hiragana[k]
-}
+import "fmt"
 
 const (
 	reset = "\033[0m"
 	bold  = "\033[1m"
 )
 
+var hiragana = map[string]string{
+	"a": "あ", "i": "い", "u": "う", "e": "え", "o": "お",
+	"ka": "か", "ki": "き", "ku": "く", "ke": "け", "ko": "こ",
+	"sa": "さ", "shi": "し", "su": "す", "se": "せ", "so": "そ",
+	"ta": "た", "chi": "ち", "tsu": "つ", "te": "て", "to": "と",
+	// TODO: NA,HA,MA,YA,RA,WA ...
+}
+
+func getRandomHiragana(next *int) (string, string) {
+	keys := make([]string, 0, len(hiragana))
+	for k := range hiragana {
+		keys = append(keys, k)
+	}
+	k := keys[*next]
+	if *next == len(keys)-1 {
+		*next = 0
+	} else {
+		*next++
+	}
+	return k, hiragana[k]
+}
+
 func main() {
 	streak := 0
+	next := 0
 	for {
-		hirKey, hirVal := getRandomHiragana()
+		hirKey, hirVal := getRandomHiragana(&next)
 		fmt.Printf("%s%s%s (streak: %d)? ", bold, hirVal, reset, streak)
 		var ans string
 		fmt.Scanln(&ans)
@@ -55,7 +47,7 @@ func main() {
 			streak++
 		} else {
 			streak = 0
-			fmt.Printf("Not quite. The correct answer is '%s%s%s'.\n", bold, hirKey, reset)
+			fmt.Printf("Incorrect! The correct answer is '%s%s%s'.\n", bold, hirKey, reset)
 		}
 	}
 }
