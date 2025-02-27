7b4c042569c5d992e75425f263123fc7de50d730
lukasjoc <45790416+lukasjoc@users.noreply.github.com>
2025-02-27 10:25:15 +0100

    gitstatus: make it work fater

--- gitstatus/main.go
+++ gitstatus/main.go
@@ -4,22 +4,26 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
+	"slices"
 	"strings"
-	"text/template"
 
 	git "github.com/go-git/go-git/v5"
 )
 
 const (
-	ansiColorBlack   = "\033[30m"
-	ansiColorRed     = "\033[31m"
-	ansiColorGreen   = "\033[32m"
-	ansiColorYellow  = "\033[33m"
-	ansiColorBlue    = "\033[34m"
-	ansiColorMagenta = "\033[35m"
-	ansiColorCyan    = "\033[36m"
-	ansiColorWhite   = "\033[37m"
-	ansiReset        = "\033[0m"
+	ansiColorRed    = "\033[31m"
+	ansiColorGreen  = "\033[32m"
+	ansiColorYellow = "\033[33m"
+	ansiColorCyan   = "\033[36m"
+	ansiColorWhite  = "\033[37m"
+	ansiReset       = "\033[0m"
+)
+
+const (
+	StatusCodeUntracked = "??"
+	StatusCodeModified  = "M"
+	StatusCodeDeleted   = "D"
+	StatusCodeRenamed   = "R"
 )
 
 type ansiText struct {
@@ -40,17 +44,18 @@ func revParseShowToplevel() (string, error) {
 	return strings.Trim(string(out), "\n"), nil
 }
 
-func main() {
-	type GitStatus struct {
-		Branch   ansiText
-		HeadHash ansiText
-		Flags    []ansiText
-	}
-	tmpl, err := template.New("gitstatus").Parse("({{.Branch}}@{{.HeadHash}}{{if gt (len .Flags) 0}};{{range .Flags}}{{.}}{{end}}{{end}})\n")
+// TODO: i need good package with git command wrappers thats not as heavy as go-git
+// and relies on the git binary itself.
+// NOTE: using this is waaay faster then worktree status from go-git. This is disappointing :(
+func status() (string, error) {
+	out, err := exec.Command("git", "status", "-s", "-u", "-M", "--porcelain").Output()
 	if err != nil {
-		panic(err)
+		return "", nil
 	}
+	return strings.Trim(string(out), "\n"), nil
+}
 
+func main() {
 	path, err := revParseShowToplevel()
 	if err != nil {
 		panic(err)
@@ -63,44 +68,44 @@ func main() {
 		panic(err)
 	}
 
-	worktree, _ := repo.Worktree()
-	status, err := worktree.Status()
+	status, err := status()
 	if err != nil {
 		panic(err)
 	}
-
-	flags := map[git.StatusCode]ansiText{}
-	// TODO: flag for if behind remote and how many commits
-	for _, stat := range status {
-		switch stat.Worktree {
-		case git.Untracked:
-			flags[git.Untracked] = ansiText{ansiColorBlue, "+"}
-		case git.Modified:
-			flags[git.Modified] = ansiText{ansiColorYellow, "*"}
-		case git.Added:
-			flags[git.Added] = ansiText{ansiColorCyan, "+"}
-		case git.Deleted:
-			flags[git.Deleted] = ansiText{ansiColorGreen, "-"}
-		case git.Renamed:
-			flags[git.Renamed] = ansiText{ansiColorYellow, "r"}
+	statuses := []string{}
+	for _, line := range strings.Split(status, "\n") {
+		l := strings.Split(strings.Trim(line, " "), " ")
+		stat := l[0]
+		if !slices.Contains(statuses, stat) {
+			statuses = append(statuses, stat)
 		}
 	}
-	flagsFormatted := []ansiText{}
-	for _, flag := range flags {
-		flagsFormatted = append(flagsFormatted, flag)
+
+	flags := []ansiText{}
+	// TODO: flag for if behind remote and how many commits
+	for _, stat := range statuses {
+		ansiStat := ansiText{ansiColorWhite, ""}
+		switch stat {
+		case StatusCodeUntracked:
+			ansiStat = ansiText{ansiColorYellow, "+"}
+		case StatusCodeModified:
+			ansiStat = ansiText{ansiColorCyan, "*"}
+		case StatusCodeDeleted:
+			ansiStat = ansiText{ansiColorRed, "-"}
+		case StatusCodeRenamed:
+			ansiStat = ansiText{ansiColorRed, "r"}
+		}
+		flags = append(flags, ansiStat)
 	}
 
+	// TODO: should replace this with git command to
 	head, err := repo.Head()
 	if err != nil {
 		panic(err)
 	}
 
-	formatted := GitStatus{
-		ansiText{ansiColorYellow, head.Name().Short()},
-		ansiText{ansiColorGreen, head.Hash().String()[0:8]},
-		flagsFormatted,
-	}
-	if err := tmpl.Execute(os.Stdout, formatted); err != nil {
-		panic(err)
-	}
+	fmt.Print(ansiText{ansiColorYellow, head.Name().Short()})
+	fmt.Print("::")
+	fmt.Print(ansiText{ansiColorGreen, head.Hash().String()[0:8]})
+	fmt.Print(flags)
 }
